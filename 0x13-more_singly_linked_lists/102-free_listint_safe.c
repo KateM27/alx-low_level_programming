5761546574ee853005596c572790aac6cc41e194
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-19 20:07:22 +0300

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,72 +1,29 @@
 #include "lists.h"
 
 /**
- * free_listp2 - frees a linked list
- * @head: start of list
- *
- * Return: Nothing
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *tmp;
-	listp_t *elem;
-
-	if (head != NULL)
-	{
-		elem = *head;
-		while ((tmp = elem) != NULL)
-		{
-			elem = elem->next;
-			free(tmp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: start of list
- *
- * Return: size of the list that was free'd.
- */
+* free_listint_safe- frees a list
+* @h: start of list
+*
+* Return: size of list that was free'd
+*/
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nodes = 0;
-	listp_t *ptr, *newNode, *addNode;
-	listint_t *elem;
+	listint_t *tmp, *elem = head;
 
-	ptr = NULL;
-	while (*h != NULL)
+	if (head == NULL)
+		return (0);
+	while (elem)
 	{
-		newNode = malloc(sizeof(listp_t));
-
-		if (newNode == NULL)
-			exit(98);
-
-		newNode->p = (void *)*h;
-		newNode->next = ptr;
-		ptr = newNode;
-
-		addNode = ptr;
-
-		while (addNode->next != NULL)
-		{
-			addNode = addNode->next;
-			if (*h == addNode->p)
-			{
-				*h = NULL;
-				free_listp2(&ptr);
-				return (nodes);
-			}
-		}
-
-		elem = *h;
-		*h = (*h)->next;
-		free(elem);
 		nodes++;
+		if ((elem->next) >= elem)
+		{
+			return (nodes);
+		}
+		tmp = elem;
+		elem = elem->next;
+		free(tmp);
 	}
-
-	*h = NULL;
-	free_listp2(&ptr);
+	*head = NULL;
 	return (nodes);
 }
