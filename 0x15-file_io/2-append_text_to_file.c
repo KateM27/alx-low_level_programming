3c9c24b25ecfd5af542a8b639a06f71267a1f000
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-25 22:33:37 +0300

    append text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* append_text_to_file- appends text at the end of file
+* @filename: name of file
+* @text_content: NULL terminated string to add at EOF
+* Return: 1 on succes, -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, apt = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	
+  fd = open(filename, O_CREAT | O_WRONLY | O_APPEND,  0600);
+	if (fd == -1)
+		return (-1);
+	
+  while (text_content[apt])
+		apt++;
+	
+  fd = write(fd, text_content, apt);
+	if (fd == -1)
+		return (-1);
+	return (1);
+}
