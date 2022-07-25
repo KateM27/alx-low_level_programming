886555ae7092f29909c00f786f67a76554f24003
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-25 22:29:27 +0300

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* create_file- creates a file
+* @filename: name of file to create
+* @text_content: NULL terminated string to write
+* Return: 1 on succes, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	
+  fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC,  0600);
+	if (fd == -1)
+		return (-1);
+	
+  while (text_content[len])
+		len++;
+	
+  fd = write(fd, text_content, len);
+	if (fd == -1)
+		return (-1);
+	return (1);
+}
