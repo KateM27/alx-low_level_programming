7990f9383a4b3a5f2aaf30ca7a65e2f9e8763930
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-25 22:35:30 +0300

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	
-  fd = open(filename, O_CREAT | O_WRONLY | O_APPEND,  0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND,  0600);
 	if (fd == -1)
 		return (-1);
 	
-  while (text_content[apt])
+	while (text_content[apt])
 		apt++;
 	
-  fd = write(fd, text_content, apt);
+	fd = write(fd, text_content, apt);
 	if (fd == -1)
 		return (-1);
 	return (1);
