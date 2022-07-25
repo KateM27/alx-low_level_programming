7bffc9125a76e72f1478fbd389b840ea3875000c
KateM27 <99427213+KateM27@users.noreply.github.com>
2022-07-25 22:23:13 +0300

    read file-POSIX

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include main.h
+
+/**
+* read_textfile- reads a text and prints it out to POSIX stdout
+* @filename: name of file to read;
+* @letters: number of letters to read and print
+* Return: the number of letters read and printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t text, fd;
+	char *buffer;
+
+	buffer= malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	
+  fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	
+  text = read(fd, buffer, letters);
+	buffer[letters] = '\0';
+	if (text == -1)
+	return (0);
+	
+  characters = write(STDOUT_FILENO, buffer, text);
+	if (text == -1)
+		return (0);
+    
+	fd = close(fd);
+	free(buffer);
+	return (text);
+}
