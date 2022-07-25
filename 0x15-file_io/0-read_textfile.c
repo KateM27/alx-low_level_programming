094fa995d2e33b39181c177ec6fac28849c02131
KateM27 <mamocatherine@gmail.com>
2022-07-25 13:17:26 -0700

    read file-posix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t text, fd;
 	char *buffer;
-	
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
