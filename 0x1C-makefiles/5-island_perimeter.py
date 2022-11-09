dabc62c649584517132a3382e6c708fcbebd3e09
KateM27 <mamocatherine@gmail.com>
2022-11-09 21:12:11 +0300

    Advanced task update

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of an island defined in grid.
+    grid is a list of list of integers"""
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid [i - 1][j] == 1):
+                    edges += 1
+    perimeter = size * 4 - edges * 2
+    return perimeter
